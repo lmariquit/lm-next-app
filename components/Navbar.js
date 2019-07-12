638f3cb8f3819277cf3444247a8a14ad725d076f
Loren Mariquit <lmariquit@dotdash.com>
2019-07-12 15:49:45 -0400

    added style jsx to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,8 +6,26 @@ const Navbar = () => (
 			<li><Link href="/"><a>HOME</a></Link></li>
 			<li><Link href="/about"><a>ABOUT</a></Link></li>
 		</ul>
-	</div>
+		<style jsx>{`
+			ul {
+				background: #333;
+				color: #fff;
+				list-style: none;
+				display: flex;
+			}
 
+			ul li {
+				font-size: 18px;
+				margin-right: 20px;
+			}
+
+			ul li a {
+				color: #fff;
+				text-decoration: none;
+			}
+
+		`}</style>
+	</div>
 );
 
 export default Navbar;
